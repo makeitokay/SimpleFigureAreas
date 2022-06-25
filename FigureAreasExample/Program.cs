72ce7e24c87f1ca77d7c7c43e43653f9219cea65
makeitokay <andrey.mu11@yandex.ru>
2022-06-25 18:03:02 +0300

    Update examples of using the library

--- FigureAreasExample/Program.cs
+++ FigureAreasExample/Program.cs
@@ -7,10 +7,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Triangle triangle = new Triangle((5, 4, 3));
-        Console.WriteLine(triangle.GetArea());
-        Console.WriteLine(triangle.IsRight);
+        List<Figure> figures = new List<Figure>();
+        for (uint i = 0; i <= 10; ++i)
+        {
+            if (i % 2 == 0)
+                figures.Add(new Triangle((i + 2, i + 4, i + 5)));
+            else
+                figures.Add(new Circle(i));
+        }
 
-        Figure circle = new Circle(5);
+        foreach (var figure in figures)
+            Console.WriteLine($"The area of {figure.ToString()} is {Math.Round(figure.GetArea(), 2)}.");
     }
 }
